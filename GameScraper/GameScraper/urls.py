2a02102401375ce9c7b1843347da99919045a8d2
Przemek <przemyslaw7wozny@gmail.com>
2020-09-14 15:36:17 +0200

    Testowy branch scrapera

--- GameScraper/GameScraper/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('frontend.urls')),
-    path('', include('games.urls'))
-]
-
--- GameScraper/games/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import AccountViewSet
-
-
-router = routers.DefaultRouter()
-router.register('api/accounts', AccountViewSet, 'accounts' )
-
-urlpatterns = router.urls
