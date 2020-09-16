af750468558c7385640ff7caebf527bd4cd2b3da
Przemek <przemyslaw7wozny@gmail.com>
2020-09-16 19:28:50 +0200

    Scheduler aktualizujacy ceny co godzine

--- apschedul.py
+++ apschedul.py
@@ -4,6 +4,8 @@ import os
 def app():
     os.system("venv\Scripts\activate & cd mysite & python manage.py crawl")
 
+app()
+
 scheduler = BlockingScheduler()
 scheduler.add_job(app, 'interval', hours=1)
 scheduler.start()
--- mysite/scrp/management/commands/crawl.py
+++ mysite/scrp/management/commands/crawl.py
@@ -4,6 +4,8 @@ from scraper.spiders.ps4 import PS4Spider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
+
+
 class Command(BaseCommand):
     help = "Release the spiders"
 
